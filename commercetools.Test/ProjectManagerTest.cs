66fbbde26623a642add700a7ba15cf0eff4cc210
Rajiv Bala <rajiv.bala@albumprinter.com>
2017-08-31 15:45:18 +0200

    porting test project to .net core

---
+++ commercetools.Test/ProjectManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using commercetools.Common;
+using commercetools.Project;
+using Xunit;
+
+namespace commercetools.Test
+{
+    /// <summary>
+    /// Test the API methods in the ProjectManager class.
+    /// </summary>
+    public class ProjectManagerTest : IDisposable
+    {
+        private Client _client;
+
+        /// <summary>
+        /// Test setup
+        /// </summary>
+        public ProjectManagerTest()
+        {
+            _client = new Client(Helper.GetConfiguration());
+        }
+
+        /// <summary>
+        /// Test teardown
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Tests the ProjectManager.GetProjectAsync method.
+        /// </summary>
+        /// <see cref="ProjectManager.GetProjectAsync"/>
+        [Fact]
+        public async Task ShouldGetProjectAsync()
+        {
+            Response<Project.Project> response = await _client.Project().GetProjectAsync();
+            Assert.True(response.Success);
+
+            Project.Project project = response.Result;
+            Assert.NotNull(project.Key);
+            Assert.NotNull(project.Name);
+            Assert.NotNull(project.CreatedAt);
+        }
+    }
+}
