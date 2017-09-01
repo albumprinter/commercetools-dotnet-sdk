f5ede539d31f2b1678d63f1481657f9f7a8e925e
Cristian Postoaca <cpost@vistaprint.com>
2017-09-01 14:39:46 +0200

    Test

--- inject_devcommerce_keys.ps1
+++ inject_devcommerce_keys.ps1
@@ -2,4 +2,4 @@ $json = Get-Content '.\commercetools.Test\appsettings.json' | Out-String | Conve
 $json.commercetoolsClientID = $env::COMMERCETOOLS_CLIENT_ID;
 $json.commercetoolsClientSecret = $env::COMMERCETOOLS_CLIENT_SECRET;
 $json | ConvertTo-Json -depth 100 | Set-Content '.\commercetools.Test\appsettings.json';
-echo Get-Content '.\commercetools.Test\appsettings.json' | Out-String;
+Get-Content '.\commercetools.Test\appsettings.json';
