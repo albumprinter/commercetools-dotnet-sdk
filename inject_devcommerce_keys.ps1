9446cb9f00a34b167fcf6776c857dbb1ea6bb791
Cristian Postoaca <cpost@vistaprint.com>
2017-09-01 14:32:27 +0200

    Create PS script to inject sensitive vars

---
+++ inject_devcommerce_keys.ps1
@@ -0,0 +1,4 @@
+$json = Get-Content '.\commercetools.Test\appsettings.json' | Out-String | ConvertFrom-Json;
+$json.commercetoolsClientID = $env::COMMERCETOOLS_CLIENT_ID;
+$json.commercetoolsClientSecret = $env::COMMERCETOOLS_CLIENT_SECRET;
+$json | ConvertTo-Json -depth 100 | Set-Content '.\commercetools.Test\appsettings.json';
