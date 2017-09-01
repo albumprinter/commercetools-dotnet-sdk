9988cdeb0e0b00854c540afdbb846cf3babb4067
Cristian Postoaca <cpost@vistaprint.com>
2017-09-01 14:43:49 +0200

    Fix PS syntax

--- inject_devcommerce_keys.ps1
+++ inject_devcommerce_keys.ps1
@@ -1,5 +1,5 @@
 $json = Get-Content '.\commercetools.Test\appsettings.json' | Out-String | ConvertFrom-Json;
-$json.commercetoolsClientID = $env::COMMERCETOOLS_CLIENT_ID;
-$json.commercetoolsClientSecret = $env::COMMERCETOOLS_CLIENT_SECRET;
+$json.commercetoolsClientID = $env:COMMERCETOOLS_CLIENT_ID;
+$json.commercetoolsClientSecret = $env:COMMERCETOOLS_CLIENT_SECRET;
 $json | ConvertTo-Json -depth 100 | Set-Content '.\commercetools.Test\appsettings.json';
 Get-Content '.\commercetools.Test\appsettings.json';
